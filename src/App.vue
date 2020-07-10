5d11355714c36aa3215fbd7c0091c8b0d4e2b939
Erik Hanchett <erik.hanchett@gmail.com>
2020-07-09 21:03:01 -0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>Parent Component</h4>
+    <h5 class="text-xl">Counter: {{ counter }}</h5>
+    <button
+      class="text-red-200 bg-red-500 rounded-md p-2 mr-4 mt-5 focus:outline-none"
+      @click="bgColor = 'bg-red-300'"
+    >Make Box Red</button>
+    <button
+      class="text-green-200 rouned-md p-2 bg-green-500 rounded focus:outline-none"
+      @click="bgColor = 'bg-green-300'"
+    >Make Box Green</button>
+    <SuperComponent id="12" :class="bgColor" @click="test" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SuperComponent from "./components/super-component";
 export default {
-  name: 'App',
+  name: "App",
+  methods: {
+    test() {
+      this.counter++;
+    }
+  },
+  data: () => ({ counter: 0, bgColor: "" }),
   components: {
-    HelloWorld
+    SuperComponent
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +37,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/super-component.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <div> -->
+  <div class="h-42 w-64 shadow-xl m-auto mt-12 rounded-md p-2">
+    <h3 class>Super Component Info</h3>
+    <div class="flex flex-col mt-5">
+      <label class="text-xs" for="box">Random Label</label>
+      <input id="box" type="text" />
+    </div>
+    <button
+      v-bind="filtered"
+      class="border-black rounded-lg p-2 border mt-5 focus:outline-none text-green-300"
+    >Press Me</button>
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  computed: {
+    filtered() {
+      const newAttrs = { ...this.$attrs };
+      delete newAttrs.class;
+      return newAttrs;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
